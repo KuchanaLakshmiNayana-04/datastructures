a8f9c864c087ea62538d3106e153e5509926d1d4
unknown <lakshminayanakuchana04@gmail.com>
2026-06-22 16:15:34 +0530

    fourth commit

--- linkedlist.py
+++ linkedlist.py
@@ -1,11 +1,11 @@
 class Node:
     def __init__(self,data):
-       self.data=  data
-       self.next = None
-class Linkedlist :
+        self.data = data
+        self.next = None
+class LinkedList:
     def __init__(self):
         self.head = None
-    def add_end(self,data):
+    def add_end(self, data):
         new = Node(data)
         if self.head is None:
             self.head = new
@@ -13,7 +13,7 @@ class Linkedlist :
         itr = self.head
         while itr.next:
             itr = itr.next
-        itr.next = new 
+        itr.next = new
     def add_beg(self,data):
         obj = Node(data)
         if self.head is None:
@@ -21,13 +21,13 @@ class Linkedlist :
             return
         obj.next = self.head
         self.head = obj
-
+        
     def display(self):
         itr = self.head
         while itr:
-            print(itr.data,end='-->')
-            itr =itr.next
-ll = Linkedlist()
+            print(itr.data,end="-->")
+            itr = itr.next
+ll = LinkedList()
 ll.add_end(50)
 ll.add_end(150)
 ll.add_end(250)
